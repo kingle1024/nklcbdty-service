e20197f19e685c7d4bf62768fcbfd34d71a7d3e4
kingle1024 <teran1024@naver.com>
2025-02-21 17:58:05 +0900

    🎉 Init: 프로젝트 초기 생성

---
+++ src/main/java/com/nklcbdty/api/NklcbdtyApplication.java
@@ -0,0 +1,13 @@
+package com.nklcbdty.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NklcbdtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NklcbdtyApplication.class, args);
+	}
+
+}
