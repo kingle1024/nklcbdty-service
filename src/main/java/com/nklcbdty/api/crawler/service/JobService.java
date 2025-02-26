002879f3b0fd7ead83325a54419487726c066878
kingle1024 <teran1024@naver.com>
2025-02-26 23:11:24 +0900

    ✨ Feat: 회사 공고 목록 가져오기 구현

---
+++ src/main/java/com/nklcbdty/api/crawler/service/JobService.java
@@ -0,0 +1,23 @@
+package com.nklcbdty.api.crawler.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nklcbdty.api.crawler.repository.JobRepository;
+import com.nklcbdty.api.crawler.vo.Job_mst;
+
+@Service
+public class JobService {
+    private final JobRepository jobRepository;
+
+    @Autowired
+    public JobService(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+    public List<Job_mst> list(String company) {
+        return jobRepository.findAllByCompanyCd(company);
+    }
+}
