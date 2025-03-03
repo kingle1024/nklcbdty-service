0d76ef280dcbc8342dcd91f3cc58fbb7e92c63f5
kingle1024 <teran1024@naver.com>
2025-03-03 19:58:53 +0900

    🔨 Refactor: 공통 메서드 분리

--- src/main/java/com/nklcbdty/api/crawler/service/NaverJobCrawlerService.java
+++ src/main/java/com/nklcbdty/api/crawler/service/NaverJobCrawlerService.java
@@ -10,7 +10,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nklcbdty.api.crawler.common.CrawlerCommonService;
@@ -24,13 +23,11 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class NaverJobCrawlerService implements JobCrawler {
 
-    private final CrawlerRepository crawlerRepository;
     private final CrawlerCommonService crawlerCommonService;
     private final String apiUrl;
 
     @Autowired
-    public NaverJobCrawlerService(CrawlerRepository crawlerRepository, CrawlerCommonService crawlerCommonService) {
-        this.crawlerRepository = crawlerRepository;
+    public NaverJobCrawlerService(CrawlerCommonService crawlerCommonService) {
         this.crawlerCommonService = crawlerCommonService;
         this.apiUrl = createApiUrl();
     }
@@ -52,38 +49,7 @@ public class NaverJobCrawlerService implements JobCrawler {
             Job_mst[] jobArray = objectMapper.readValue(jobList.toString(), Job_mst[].class);
             result = new ArrayList<>(List.of(jobArray));
 
-            // 모든 기존 데이터 조회
-            List<Long> annoIds = result.stream().map(Job_mst::getAnnoId).collect(Collectors.toList());
-            List<Job_mst> existingJobs = crawlerRepository.findAllByAnnoIdIn(annoIds); // 기존 Job_mst 조회
-
-            List<Job_mst> jobsToSave = new ArrayList<>();
-
-            for (Job_mst job : result) {
-                boolean exists = existingJobs.stream().anyMatch(e -> e.getAnnoId().equals(job.getAnnoId()));
-
-                if (exists) {
-                    // 기존 데이터가 존재하는 경우
-                    Job_mst existingJob = existingJobs.stream()
-                            .filter(e -> e.getAnnoId().equals(job.getAnnoId()))
-                            .findFirst()
-                            .orElse(null);
-                    if (existingJob != null && !existingJob.getAnnoSubject().equals(job.getAnnoSubject())) {
-                        // annoSubject가 다를 경우에만 저장
-                        jobsToSave.add(job);
-                    }
-                } else {
-                    // 존재하지 않으면 새로 저장
-                    jobsToSave.add(job);
-                }
-            }
-
-            // 한 번에 저장
-            if (!jobsToSave.isEmpty()) {
-                for (Job_mst item : jobsToSave) {
-                    item.setCompanyCd("NAVER");
-                }
-                crawlerRepository.saveAll(jobsToSave);
-            }
+            crawlerCommonService.saveAll("NAVER", result);
 
         } catch (Exception e) {
             log.error("Error occurred while crawling jobs: {}", e.getMessage(), e);
