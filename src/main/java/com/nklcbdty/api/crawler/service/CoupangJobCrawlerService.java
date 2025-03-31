93131e53906c5da91771b64d05244875aa1a0aa8
kingle1024 <teran1024@naver.com>
2025-03-31 22:38:47 +0900

    🚨 Fix(#14): 쿠팡 크롤링 안되는 현상 수정

--- src/main/java/com/nklcbdty/api/crawler/service/CoupangJobCrawlerService.java
+++ src/main/java/com/nklcbdty/api/crawler/service/CoupangJobCrawlerService.java
@@ -32,7 +32,7 @@ public class CoupangJobCrawlerService implements JobCrawler{
 	}
 
     private String getApiUrl() {
-    	return "https://www.coupang.jobs/kr/jobs";
+    	return "https://www.coupang.jobs/kr/jobs/?orderby=0&pagesize=20&radius=100&location=Seoul,%20South%20Korea#results";
     }
 
     @Override
@@ -49,7 +49,8 @@ public class CoupangJobCrawlerService implements JobCrawler{
             == 0 ? (double)(totalCnt / pagesize) : Math.ceil((double)totalCnt / pagesize);
 
 		for (int i = 1; i <= (int)totalLoopCnt; i++) {
-			apiUrl = "https://www.coupang.jobs/kr/jobs?page="+i+"#results";
+			// apiUrl = "https://www.coupang.jobs/kr/jobs?page="+i+"#results";
+			apiUrl = "https://www.coupang.jobs/kr/jobs/?page="+i+"&orderby=0&pagesize=20&radius=100&location=Seoul,%20South%20Korea#results";
 			resList.addAll(coupangParseHtmlData(apiUrl));
             log.info("{} / {} 크롤링 완료", i, totalLoopCnt);
 		    try {
@@ -80,7 +81,8 @@ public class CoupangJobCrawlerService implements JobCrawler{
 		
         try {
         	Document doc = Jsoup.connect(apiUrl)
-                .header("User-Agent", "PostmanRuntime/7.43.2") // User-Agent 설정
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36") // User-Agent 설정
+                .header("Cookie", "ph_cookiePref=NFAT; _gcl_au=1.1.160249659.1740312267; _fbp=fb.1.1740312267141.693781373128074153; __Host-vId=8465453b-8366-43f0-9eb3-28f53a0a4d7f; UMB_SESSION=CfDJ8M8dnfYreA1GpSGqsJMV%2Fne4R874RnIPG4Yp8b9jCiHmki4Wh7FVEB93%2BmLAcQbDqHih7Kty7GoinxvrY84UJd9DMz8GMpe9mvir0tzDjZWY9Z4m0N1W9O2lSy8zp4jFVKJNgOAy36P5cngYOQK9yIqRmRJPiCwoYrw8rqwtOOYU; _clck=tp51lh%7C2%7Cfuo%7C0%7C1880; _gid=GA1.2.13238582.1743427274; _ga=GA1.1.1363205650.1740312267; _clsk=z9ir1d%7C1743427383507%7C4%7C1%7Co.clarity.ms%2Fcollect; _ga_WN9DBP9Q8X=GS1.1.1743427274.12.1.1743427388.53.0.0")
                 .get();
         	Elements root = doc.select("main#content div#js-job-search-results .card.card-job");
         	
@@ -125,7 +127,8 @@ public class CoupangJobCrawlerService implements JobCrawler{
 
     	try {
             Document doc = Jsoup.connect(apiUrl)
-                                .header("User-Agent", "PostmanRuntime/7.43.2") // User-Agent 설정
+                                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36") // User-Agent 설정
+                                .header("Cookie", "ph_cookiePref=NFAT; _gcl_au=1.1.160249659.1740312267; _fbp=fb.1.1740312267141.693781373128074153; __Host-vId=8465453b-8366-43f0-9eb3-28f53a0a4d7f; UMB_SESSION=CfDJ8M8dnfYreA1GpSGqsJMV%2Fne4R874RnIPG4Yp8b9jCiHmki4Wh7FVEB93%2BmLAcQbDqHih7Kty7GoinxvrY84UJd9DMz8GMpe9mvir0tzDjZWY9Z4m0N1W9O2lSy8zp4jFVKJNgOAy36P5cngYOQK9yIqRmRJPiCwoYrw8rqwtOOYU; _clck=tp51lh%7C2%7Cfuo%7C0%7C1880; _gid=GA1.2.13238582.1743427274; _ga=GA1.1.1363205650.1740312267; _clsk=z9ir1d%7C1743427383507%7C4%7C1%7Co.clarity.ms%2Fcollect; _ga_WN9DBP9Q8X=GS1.1.1743427274.12.1.1743427388.53.0.0")
                                 .get(); // GET 요청
 
     		// 총건수 파싱하기.
