c3bdc1a6467c6288abed4ddab3989a76eb76afd9
kingle1024 <teran1024@naver.com>
2025-04-18 19:36:38 +0900

    ✨ Feat: 방문자 날짜,갯수 API 추가

--- src/main/java/com/nklcbdty/api/statistics/controller/StatisticsController.java
+++ src/main/java/com/nklcbdty/api/statistics/controller/StatisticsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nklcbdty.api.log.service.VisitorService;
 import com.nklcbdty.api.statistics.service.StatisticsService;
 
 @RestController
@@ -14,14 +15,16 @@ import com.nklcbdty.api.statistics.service.StatisticsService;
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
+    private final VisitorService visitorService;
 
     @Autowired
-    public StatisticsController(StatisticsService statisticsService) {
+    public StatisticsController(StatisticsService statisticsService, VisitorService visitorService) {
         this.statisticsService = statisticsService;
+        this.visitorService = visitorService;
     }
 
-    @GetMapping("/list")
-    public ResponseEntity<?> list() {
+    @GetMapping("")
+    public ResponseEntity<?> visitors() {
         try {
             return ResponseEntity.ok(statisticsService.getVisitorList());
         } catch (RuntimeException e) {
@@ -29,4 +32,12 @@ public class StatisticsController {
         }
     }
 
+    @GetMapping("/count-by-date")
+    public ResponseEntity<?> countByDate() {
+        try {
+            return ResponseEntity.ok(visitorService.getCountsByDate());
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(401).body(e.getMessage());
+        }
+    }
 }
