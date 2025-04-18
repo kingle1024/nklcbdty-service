7b142ef21847bd3da56a63d18f224ac4ac127317
kingle1024 <teran1024@naver.com>
2025-04-18 19:36:38 +0900

    ✨ Feat: 방문자 날짜,갯수 API 추가

--- src/main/java/com/nklcbdty/api/log/repository/VisitorRepository.java
+++ src/main/java/com/nklcbdty/api/log/repository/VisitorRepository.java
@@ -1,8 +1,9 @@
 package com.nklcbdty.api.log.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import com.nklcbdty.api.log.entity.VisitorEntity;
 
-public interface VisitorRepository extends JpaRepository<VisitorEntity, Long> {
+public interface VisitorRepository extends JpaRepository<VisitorEntity, Long>, QuerydslPredicateExecutor<VisitorEntity>, VisitorRepositoryCustom {
 }
