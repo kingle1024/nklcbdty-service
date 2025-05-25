cc5167915038a4b2fd15fdb2070c4a8c7cb74386
kingle1024 <teran1024@naver.com>
2025-05-25 20:32:22 +0900

    🚨 Fix: 마감일이 근접한 공고가 상단에 표시되도록 수정

--- src/main/java/com/nklcbdty/api/crawler/repository/JobRepository.java
+++ src/main/java/com/nklcbdty/api/crawler/repository/JobRepository.java
@@ -9,6 +9,6 @@ import com.nklcbdty.api.crawler.vo.Job_mst;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job_mst, Long> {
-    List<Job_mst> findAllByCompanyCdAndSubJobCdNmIsNotNull(String company);
+    List<Job_mst> findAllByCompanyCdAndSubJobCdNmIsNotNullOrderByEndDateAsc(String company);
     List<Job_mst> findAllBySubJobCdNmIsNotNull();
 }
