57d6b6c88d45f1143fa8c5437c1330445c508a17
kingle1024 <teran1024@naver.com>
2025-05-26 23:05:32 +0900

    ✨ Feat: JWT 카카오 로그인 기능 구현 grdle 추가, Redis 적용

---
+++ src/main/java/com/nklcbdty/api/auth/service/UserDetailService.java
@@ -0,0 +1,8 @@
+package com.nklcbdty.api.auth.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UserDetailService {
+    UserDetails loadUserById(String userId, String username, String refreshToken) throws UsernameNotFoundException;
+}
