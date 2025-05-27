fdfc3578317d8337782410e1b41b895c17055c5e
kingle1024 <teran1024@naver.com>
2025-05-27 10:15:40 +0900

    ✨ Feat: Spring Security 적용

---
+++ src/main/java/com/nklcbdty/api/common/security/AllowedPaths.java
@@ -0,0 +1,28 @@
+package com.nklcbdty.api.common.security;
+
+import java.util.Arrays;
+
+import lombok.Getter;
+
+@Getter
+public enum AllowedPaths {
+    LOGIN("/login"),
+    KAKAO_LOGIN("/api/kakaoLogin"),
+    OAUTH2("/oauth2/**"),
+    DETAIL("/detail/**"),
+    LIST("/list"),
+    EMAIL("/email/send")
+    ;
+
+    private final String path;
+
+    AllowedPaths(String path) {
+        this.path = path;
+    }
+
+    public static String[] getAllowedPaths() {
+        return Arrays.stream(AllowedPaths.values())
+                     .map(AllowedPaths::getPath)
+                     .toArray(String[]::new);
+    }
+}
