8fc450573033ba8f7005b52696c712f6ceb24219
kingle1024 <teran1024@naver.com>
2026-05-31 20:17:54 +0900

    ♻️ Refactor: JobRepository, CrawlerRepository 를 nklcbdty-common 으로 이전 (0.10.0)

--- src/main/java/com/nklcbdty/api/crawler/service/JobService.java
+++ src/main/java/com/nklcbdty/api/crawler/service/JobService.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.nklcbdty.api.crawler.repository.JobRepository;
+import com.nklcbdty.common.crawler.repository.JobRepository;
 import com.nklcbdty.common.vo.Job_mst;
 
 import lombok.extern.slf4j.Slf4j;
